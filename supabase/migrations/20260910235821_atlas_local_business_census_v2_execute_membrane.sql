96e22123aa98bc2577f5a42ed1d3e0d3e8942932
optical-lift <gleamingbrook@gmail.com>
2026-09-10 18:58:36 -0500

    Restore Atlas Local census v2 execute membrane migration source

---
+++ supabase/migrations/20260910235821_atlas_local_business_census_v2_execute_membrane.sql
@@ -0,0 +1,2 @@
+revoke all on function local_intel.claim_business_census_work_v2(uuid, integer) from public, anon, authenticated;
+grant execute on function local_intel.claim_business_census_work_v2(uuid, integer) to service_role;
